b82bee292c7dd3d862b8eb6ad59f44e23633b408
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-04 21:55:01 +0300

    Create Assembly-CSharp_CodeGen.c

---
+++ Roll-a-Ball-Game/Builds/Roll-a-Ball_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,175 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void CameraController::Start()
+extern void CameraController_Start_m92C8EBBE34DA314CE0C730E07400A96E968264BB (void);
+// 0x00000002 System.Void CameraController::LateUpdate()
+extern void CameraController_LateUpdate_m3B932A865EC4F153295CA8345D332060035F6D00 (void);
+// 0x00000003 System.Void CameraController::.ctor()
+extern void CameraController__ctor_m6E88C7769B5C6E7CC68A3670C07050374E05F902 (void);
+// 0x00000004 System.Void EnemyMover::Update()
+extern void EnemyMover_Update_m95B4E8A74FDFA0CCCFDCC9D24ED91633FDB26150 (void);
+// 0x00000005 System.Void EnemyMover::.ctor()
+extern void EnemyMover__ctor_m1DE1AFF3FC2E14401CC02726819DD31DDFBFBC9A (void);
+// 0x00000006 System.Void FloorMover::FixedUpdate()
+extern void FloorMover_FixedUpdate_mC9341EC37391B258586176C303D3AFA6A768216B (void);
+// 0x00000007 System.Void FloorMover::OnCollisionEnter(UnityEngine.Collision)
+extern void FloorMover_OnCollisionEnter_mDA14AB3ABBAFDA09263F23A94F16098F2FD82B80 (void);
+// 0x00000008 System.Void FloorMover::.ctor()
+extern void FloorMover__ctor_m45610F23D626BE627FBBA328E880BD4835E48755 (void);
+// 0x00000009 System.Void PlaneMover::Start()
+extern void PlaneMover_Start_m348E803AA98583C15A8F002E59C359699CC1E224 (void);
+// 0x0000000A System.Void PlaneMover::FixedUpdate()
+extern void PlaneMover_FixedUpdate_m72611C77969FAAA0BED273D2C911D56B65CF2E45 (void);
+// 0x0000000B System.Void PlaneMover::.ctor()
+extern void PlaneMover__ctor_mCC88F66EF239F6380DCBA58EDC69D59B06C91198 (void);
+// 0x0000000C System.Void PlayerController::Start()
+extern void PlayerController_Start_m37E4A9612CC78B422FBFC15D257767AE6F969D78 (void);
+// 0x0000000D System.Void PlayerController::OnMove(UnityEngine.InputSystem.InputValue)
+extern void PlayerController_OnMove_m95A2B12CB87C6860E42A721E5A1C816A9FF8AB56 (void);
+// 0x0000000E System.Void PlayerController::SetCountText()
+extern void PlayerController_SetCountText_mAE58139E9F5AA2D4AC147E2DB03D5CA28A06AF65 (void);
+// 0x0000000F System.Void PlayerController::FixedUpdate()
+extern void PlayerController_FixedUpdate_m16AA033B467FF810A9C1151B616BC1D4B74F7A0E (void);
+// 0x00000010 System.Void PlayerController::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerController_OnTriggerEnter_m1310BAC5FA7FAE003E04BFDB6173885B41D826E7 (void);
+// 0x00000011 System.Void PlayerController::resetPlayer()
+extern void PlayerController_resetPlayer_m81FD3F1A89F46409BA96FA2B55969008204F6E08 (void);
+// 0x00000012 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mF66473FEFF2DFEB0325A4D9C917C27C6CE4B22F7 (void);
+// 0x00000013 System.Void Rotator::Update()
+extern void Rotator_Update_m5EEA78DA43EE3059DD3A2B5E0A378709E0DF21FA (void);
+// 0x00000014 System.Void Rotator::.ctor()
+extern void Rotator__ctor_m6FA12381051396D6275FE1B56FBD254201D5B127 (void);
+// 0x00000015 System.Void SpinnerMover::Update()
+extern void SpinnerMover_Update_mB45026E585FFB7704BCDF390E9F95E0D56A444D3 (void);
+// 0x00000016 System.Void SpinnerMover::.ctor()
+extern void SpinnerMover__ctor_m9538C304366054D9763700C76CF9F4D12C531B2B (void);
+// 0x00000017 System.Void Readme::.ctor()
+extern void Readme__ctor_m23AE6143BDABB863B629ADE701E2998AB8651D4C (void);
+// 0x00000018 System.Void UnityTemplateProjects.SimpleCameraController::OnEnable()
+extern void SimpleCameraController_OnEnable_mE3D6E47455F101F2DEEBC2A58D09A97CF38E80B8 (void);
+// 0x00000019 UnityEngine.Vector3 UnityTemplateProjects.SimpleCameraController::GetInputTranslationDirection()
+extern void SimpleCameraController_GetInputTranslationDirection_m73C99DB69CEB467834BBA00A62415D1CEEF0CB47 (void);
+// 0x0000001A System.Void UnityTemplateProjects.SimpleCameraController::Update()
+extern void SimpleCameraController_Update_mBCD24408A4A2C4053F2F98DB808BD6DE88CA998F (void);
+// 0x0000001B System.Void UnityTemplateProjects.SimpleCameraController::.ctor()
+extern void SimpleCameraController__ctor_m8DE12FC1A6C31D2D60ED78F0B574CE3F864F546E (void);
+// 0x0000001C System.Void Readme_Section::.ctor()
+extern void Section__ctor_mE73C1D6AE5454B5A67AAB04CAA5144A5CA0B0D96 (void);
+// 0x0000001D System.Void UnityTemplateProjects.SimpleCameraController_CameraState::SetFromTransform(UnityEngine.Transform)
+extern void CameraState_SetFromTransform_m6467352ED87301E5F4A76456060A765CAB96AF3E (void);
+// 0x0000001E System.Void UnityTemplateProjects.SimpleCameraController_CameraState::Translate(UnityEngine.Vector3)
+extern void CameraState_Translate_m76BCC104A48EA7F125D5A50D874A2DEEA7967247 (void);
+// 0x0000001F System.Void UnityTemplateProjects.SimpleCameraController_CameraState::LerpTowards(UnityTemplateProjects.SimpleCameraController_CameraState,System.Single,System.Single)
+extern void CameraState_LerpTowards_m883AAF2D3C7F5045B64CAF655FB84EF0FC98F282 (void);
+// 0x00000020 System.Void UnityTemplateProjects.SimpleCameraController_CameraState::UpdateTransform(UnityEngine.Transform)
+extern void CameraState_UpdateTransform_mE3349362276789C1617C01276F7DE533BBA22623 (void);
+// 0x00000021 System.Void UnityTemplateProjects.SimpleCameraController_CameraState::.ctor()
+extern void CameraState__ctor_m4A83DF36C7D280050EA1B101E61B7E345C31A322 (void);
+static Il2CppMethodPointer s_methodPointers[33] = 
+{
+	CameraController_Start_m92C8EBBE34DA314CE0C730E07400A96E968264BB,
+	CameraController_LateUpdate_m3B932A865EC4F153295CA8345D332060035F6D00,
+	CameraController__ctor_m6E88C7769B5C6E7CC68A3670C07050374E05F902,
+	EnemyMover_Update_m95B4E8A74FDFA0CCCFDCC9D24ED91633FDB26150,
+	EnemyMover__ctor_m1DE1AFF3FC2E14401CC02726819DD31DDFBFBC9A,
+	FloorMover_FixedUpdate_mC9341EC37391B258586176C303D3AFA6A768216B,
+	FloorMover_OnCollisionEnter_mDA14AB3ABBAFDA09263F23A94F16098F2FD82B80,
+	FloorMover__ctor_m45610F23D626BE627FBBA328E880BD4835E48755,
+	PlaneMover_Start_m348E803AA98583C15A8F002E59C359699CC1E224,
+	PlaneMover_FixedUpdate_m72611C77969FAAA0BED273D2C911D56B65CF2E45,
+	PlaneMover__ctor_mCC88F66EF239F6380DCBA58EDC69D59B06C91198,
+	PlayerController_Start_m37E4A9612CC78B422FBFC15D257767AE6F969D78,
+	PlayerController_OnMove_m95A2B12CB87C6860E42A721E5A1C816A9FF8AB56,
+	PlayerController_SetCountText_mAE58139E9F5AA2D4AC147E2DB03D5CA28A06AF65,
+	PlayerController_FixedUpdate_m16AA033B467FF810A9C1151B616BC1D4B74F7A0E,
+	PlayerController_OnTriggerEnter_m1310BAC5FA7FAE003E04BFDB6173885B41D826E7,
+	PlayerController_resetPlayer_m81FD3F1A89F46409BA96FA2B55969008204F6E08,
+	PlayerController__ctor_mF66473FEFF2DFEB0325A4D9C917C27C6CE4B22F7,
+	Rotator_Update_m5EEA78DA43EE3059DD3A2B5E0A378709E0DF21FA,
+	Rotator__ctor_m6FA12381051396D6275FE1B56FBD254201D5B127,
+	SpinnerMover_Update_mB45026E585FFB7704BCDF390E9F95E0D56A444D3,
+	SpinnerMover__ctor_m9538C304366054D9763700C76CF9F4D12C531B2B,
+	Readme__ctor_m23AE6143BDABB863B629ADE701E2998AB8651D4C,
+	SimpleCameraController_OnEnable_mE3D6E47455F101F2DEEBC2A58D09A97CF38E80B8,
+	SimpleCameraController_GetInputTranslationDirection_m73C99DB69CEB467834BBA00A62415D1CEEF0CB47,
+	SimpleCameraController_Update_mBCD24408A4A2C4053F2F98DB808BD6DE88CA998F,
+	SimpleCameraController__ctor_m8DE12FC1A6C31D2D60ED78F0B574CE3F864F546E,
+	Section__ctor_mE73C1D6AE5454B5A67AAB04CAA5144A5CA0B0D96,
+	CameraState_SetFromTransform_m6467352ED87301E5F4A76456060A765CAB96AF3E,
+	CameraState_Translate_m76BCC104A48EA7F125D5A50D874A2DEEA7967247,
+	CameraState_LerpTowards_m883AAF2D3C7F5045B64CAF655FB84EF0FC98F282,
+	CameraState_UpdateTransform_mE3349362276789C1617C01276F7DE533BBA22623,
+	CameraState__ctor_m4A83DF36C7D280050EA1B101E61B7E345C31A322,
+};
+static const int32_t s_InvokerIndices[33] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	26,
+	23,
+	23,
+	23,
+	23,
+	23,
+	26,
+	23,
+	23,
+	26,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	1134,
+	23,
+	23,
+	23,
+	26,
+	1135,
+	1307,
+	26,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	33,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
