4f0f8a7e6eab49c2f04cbc4d24e77e574afbc8a7
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-04 21:31:08 +0300

    Create SpinnerMover.cs

---
+++ Roll-a-Ball-Game/Scripts/SpinnerMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerMover : MonoBehaviour
+{
+    public float spinSpeed = 90;
+    public float direction = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime*spinSpeed*direction);
+    }
+}
