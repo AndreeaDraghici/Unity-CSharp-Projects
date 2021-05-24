4a22b3ab9cd2358773ce9629a0b86df0e59ae4ec
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-05-24 08:18:13 +0300

    Add files via upload

---
+++ JohnLemon’s HauntedJaunt-3D Beginner/Scripts/Invisibility.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisibility : MonoBehaviour
+{
+    private SpriteRenderer character;
+    private Color col;
+    private float activationTime;
+    private bool invisible;
+    
+    void Start()
+    {
+        character = GetComponent<SpriteRenderer>();
+        activationTime = 0;
+        invisible = false;
+        col = character.color;
+    }
+
+    
+    void Update()
+    {
+        activationTime += Time.deltaTime;
+        if(invisible && activationTime >=3)
+        {
+            invisible = false;
+            col.a= 1;
+            character.color = col;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Invisible")
+        {
+            invisible = true;
+            activationTime = 0;
+            col.a = .2f;
+            character.color = col;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
